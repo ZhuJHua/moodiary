1195a5d105ade4a408cc2123b75bce8af921e983
ZhuJHua <1624109111@qq.com>
2024-11-20 06:13:42 +0800

    refactor(ui): improve ui

--- lib/pages/analyse/analyse_view.dart
+++ lib/pages/analyse/analyse_view.dart
@@ -16,142 +16,122 @@ class AnalysePage extends StatelessWidget {
     final state = Bind.find<AnalyseLogic>().state;
     final textStyle = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
+    final size = MediaQuery.sizeOf(context);
 
     //柱状图
-    Widget buildBarChart(
-        String title, Map<String, IconData> iconMap, Map<String, int> countMap, List<String> itemList) {
+    Widget buildBarChart(Map<String, IconData> iconMap, Map<String, int> countMap, List<String> itemList) {
       //去重
       itemList = Utils().arrayUtil.toSetList(itemList);
-      return Container(
-        padding: const EdgeInsets.all(20.0),
-        margin: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        decoration:
-            BoxDecoration(color: colorScheme.surfaceContainerHighest, borderRadius: BorderRadius.circular(10.0)),
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: textStyle.titleMedium!.copyWith(color: colorScheme.onSurface),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            AspectRatio(
-              aspectRatio: 1.0,
-              child: itemList.isNotEmpty
-                  ? BarChart(
-                      BarChartData(
-                        alignment: BarChartAlignment.spaceAround,
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border.symmetric(
-                            horizontal: BorderSide(
-                              color: colorScheme.onSurface.withAlpha((255 * 0.6).toInt()),
-                            ),
+      return Card.filled(
+        color: colorScheme.surfaceContainer,
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: itemList.isNotEmpty
+                ? BarChart(
+                    BarChartData(
+                      alignment: BarChartAlignment.spaceAround,
+                      borderData: FlBorderData(
+                        show: true,
+                        border: Border.symmetric(
+                          horizontal: BorderSide(
+                            color: colorScheme.onSurface.withAlpha((255 * 0.6).toInt()),
                           ),
-                        ),
-                        titlesData: FlTitlesData(
-                          leftTitles: AxisTitles(
-                              sideTitles: SideTitles(
-                                  showTitles: true,
-                                  interval: 1.0,
-                                  getTitlesWidget: (value, meta) {
-                                    return Text(value.toInt().toString());
-                                  })),
-                          bottomTitles: AxisTitles(
-                            sideTitles: SideTitles(
-                                showTitles: true,
-                                getTitlesWidget: (value, meta) {
-                                  return SideTitleWidget(
-                                      axisSide: meta.axisSide, child: Icon(iconMap[itemList[value.toInt()]]));
-                                }),
-                          ),
-                          topTitles: const AxisTitles(),
-                          rightTitles: const AxisTitles(),
-                        ),
-                        gridData: FlGridData(
-                          show: true,
-                          drawVerticalLine: false,
-                          checkToShowHorizontalLine: (value) {
-                            return value.toInt() == value;
-                          },
-                          getDrawingHorizontalLine: (value) {
-                            return FlLine(
-                              color: colorScheme.onSurface.withAlpha((255 * 0.2).toInt()),
-                              strokeWidth: 1,
-                            );
-                          },
-                        ),
-                        barGroups: List.generate(
-                          itemList.length,
-                          (index) => BarChartGroupData(x: index, barRods: [
-                            BarChartRodData(
-                                fromY: 0, toY: countMap[itemList[index]]!.toDouble(), color: colorScheme.primary)
-                          ]),
                         ),
                       ),
-                    )
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (state.finished) ...[
-                          Text(
-                            '暂无数据',
-                            style: textStyle.titleLarge!.copyWith(color: colorScheme.onSurface),
-                          ),
-                        ] else ...[
-                          const CircularProgressIndicator(),
-                        ],
-                      ],
+                      titlesData: FlTitlesData(
+                        leftTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                                showTitles: true,
+                                interval: 1.0,
+                                getTitlesWidget: (value, meta) {
+                                  return Text(value.toInt().toString());
+                                })),
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (value, meta) {
+                                return SideTitleWidget(
+                                    axisSide: meta.axisSide, child: Icon(iconMap[itemList[value.toInt()]]));
+                              }),
+                        ),
+                        topTitles: const AxisTitles(),
+                        rightTitles: const AxisTitles(),
+                      ),
+                      gridData: FlGridData(
+                        show: true,
+                        drawVerticalLine: false,
+                        checkToShowHorizontalLine: (value) {
+                          return value.toInt() == value;
+                        },
+                        getDrawingHorizontalLine: (value) {
+                          return FlLine(
+                            color: colorScheme.onSurface.withAlpha((255 * 0.2).toInt()),
+                            strokeWidth: 1,
+                          );
+                        },
+                      ),
+                      barGroups: List.generate(
+                        itemList.length,
+                        (index) => BarChartGroupData(x: index, barRods: [
+                          BarChartRodData(
+                              fromY: 0, toY: countMap[itemList[index]]!.toDouble(), color: colorScheme.primary)
+                        ]),
+                      ),
                     ),
-            )
-          ],
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (state.finished) ...[
+                        Text(
+                          '暂无数据',
+                          style: textStyle.titleLarge!.copyWith(color: colorScheme.onSurface),
+                        ),
+                      ] else ...[
+                        const CircularProgressIndicator(),
+                      ],
+                    ],
+                  ),
+          ),
         ),
       );
     }
 
-    Widget buildMoodWrap(String title, List<double> itemList) {
-      return Container(
-        padding: const EdgeInsets.all(20.0),
-        margin: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        decoration:
-            BoxDecoration(color: colorScheme.surfaceContainerHighest, borderRadius: BorderRadius.circular(10.0)),
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: textStyle.titleMedium!.copyWith(color: colorScheme.onSurface),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            AspectRatio(
-              aspectRatio: 1.0,
-              child: itemList.isNotEmpty
-                  ? SingleChildScrollView(
+    Widget buildMoodWrap(List<double> itemList) {
+      return Card.filled(
+        color: colorScheme.surfaceContainer,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: itemList.isNotEmpty
+                ? Center(
+                    child: SingleChildScrollView(
                       child: Wrap(
-                        spacing: 10.0,
-                        runSpacing: 10.0,
+                        spacing: 8.0,
+                        runSpacing: 8.0,
                         children: List.generate(state.moodList.length, (index) {
                           return MoodIconComponent(value: state.moodList[index]);
                         }),
                       ),
-                    )
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (state.finished) ...[
-                          Text(
-                            '暂无数据',
-                            style: textStyle.titleLarge!.copyWith(color: colorScheme.onSurface),
-                          ),
-                        ] else ...[
-                          const CircularProgressIndicator(),
-                        ],
-                      ],
                     ),
-            ),
-          ],
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (state.finished) ...[
+                        Text(
+                          '暂无数据',
+                          style: textStyle.titleLarge!.copyWith(color: colorScheme.onSurface),
+                        ),
+                      ] else ...[
+                        const CircularProgressIndicator(),
+                      ],
+                    ],
+                  ),
+          ),
         ),
       );
     }
@@ -164,48 +144,54 @@ class AnalysePage extends StatelessWidget {
       ),
       body: GetBuilder<AnalyseLogic>(builder: (_) {
         return ListView(
+          padding: const EdgeInsets.all(4.0),
           children: [
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              decoration:
-                  BoxDecoration(color: colorScheme.surfaceContainerHighest, borderRadius: BorderRadius.circular(10.0)),
-              child: Row(
-                children: [
-                  IconButton.filled(
-                      onPressed: () {
-                        logic.openDatePicker(context);
-                      },
-                      icon: const Icon(Icons.date_range)),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: Text(
+            Card.filled(
+              color: colorScheme.surfaceContainer,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  spacing: 8.0,
+                  children: [
+                    IconButton.filled(
+                        onPressed: () {
+                          logic.openDatePicker(context);
+                        },
+                        icon: const Icon(Icons.date_range)),
+                    Text(
                       '${state.dateRange[0].year}年${state.dateRange[0].month}月${state.dateRange[0].day}日 至 ${state.dateRange[1].year}年${state.dateRange[1].month}月${state.dateRange[1].day}日',
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              decoration:
-                  BoxDecoration(color: colorScheme.surfaceContainerHighest, borderRadius: BorderRadius.circular(10.0)),
-              child: Column(
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        logic.getAi();
-                      },
-                      child: const Text('AI 分析')),
-                  if (state.reply != '') ...[Text(state.reply)]
-                ],
+            Card.filled(
+              color: colorScheme.surfaceContainer,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          logic.getAi();
+                        },
+                        child: const Text('AI 分析')),
+                    if (state.reply != '') ...[Text(state.reply)]
+                  ],
+                ),
               ),
             ),
-            buildBarChart('天气', WeatherIcon.map, state.weatherMap, state.weatherList),
-            buildMoodWrap('心情', state.moodList),
+            GridView.count(
+              crossAxisCount: size.width > 700 ? 2 : 1,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.zero,
+              children: [
+                buildBarChart(WeatherIcon.map, state.weatherMap, state.weatherList),
+                buildMoodWrap(state.moodList)
+              ],
+            ),
           ],
         );
       }),
