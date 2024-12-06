61c8341e8c03d5f8b63c9c25a3950c0c544cf8fc
ZhuJHua <1624109111@qq.com>
2024-12-06 23:56:35 +0800

    refactor(*): use the new flutter api to catch error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,7 @@ Future<void> initSystem() async {
   //地图缓存
   await FMTCObjectBoxBackend().initialise();
   await const FMTCStore('mapStore').manage.create();
-  await RustLib.init();
+  unawaited(RustLib.init());
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       systemNavigationBarColor: Colors.transparent, systemNavigationBarContrastEnforced: false));
@@ -61,58 +62,43 @@ String getInitialRoute() {
   return AppRoutes.homePage;
 }
 
-void main() {
-  runZonedGuarded(() {
-    FlutterError.onError = (details) {
-      Utils().logUtil.printError('Flutter error', error: details.exception, stackTrace: details.stack);
-      if (details.exceptionAsString().contains('Render')) {
-        Utils().noticeUtil.showBug(message: '布局异常！');
-      } else {
-        Utils().noticeUtil.showBug(message: '出错了，请联系开发者！');
-      }
-    };
-    initSystem().then((_) {
-      return runApp(GetMaterialApp(
-        initialRoute: getInitialRoute(),
-        onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
-        builder: (context, child) {
-          final fontScale = Utils().prefUtil.getValue<double>('fontScale')!;
-          final colorScheme = Theme.of(context).colorScheme;
-          final mediaQuery = MediaQuery(
-            data: MediaQuery.of(context).copyWith(
-              textScaler: TextScaler.linear(fontScale),
-            ),
-            child: FToastBuilder()(context, child!),
-          );
-          // 根据平台决定是否需要 MoveWindow
-          var autoFit = AnnotatedRegion<SystemUiOverlayStyle>(
-              value: SystemUiOverlayStyle(
-                systemNavigationBarColor: colorScheme.surface,
-              ),
-              child: mediaQuery);
-          final windowChild = (Platform.isWindows || Platform.isMacOS || Platform.isLinux)
-              ? Column(
-                  children: [
-                    WindowButtons(
-                      colorScheme: colorScheme,
-                    ),
-                    Expanded(child: mediaQuery),
-                  ],
-                )
-              : mediaQuery;
-          return windowChild;
-        },
-        theme: Utils().themeUtil.buildTheme(Brightness.light),
-        darkTheme: Utils().themeUtil.buildTheme(Brightness.dark),
-        themeMode: ThemeMode.values[Utils().prefUtil.getValue<int>('themeMode')!],
-        defaultTransition: Transition.cupertino,
-        getPages: AppPages.routes,
-        localizationsDelegates: const [...AppLocalizations.localizationsDelegates, FlutterQuillLocalizations.delegate],
-        supportedLocales: AppLocalizations.supportedLocales,
-      ));
-    });
-  }, (error, stack) {
+void main() async {
+  await initSystem();
+  FlutterError.onError = (details) {
+    Utils().logUtil.printError('Flutter error', error: details.exception, stackTrace: details.stack);
+    if (details.exceptionAsString().contains('Render')) {
+      Utils().noticeUtil.showBug(message: '布局异常！');
+    } else {
+      Utils().noticeUtil.showBug(message: '出错了，请联系开发者！');
+    }
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
     Utils().logUtil.printWTF('Error', error: error, stackTrace: stack);
     Utils().noticeUtil.showBug(message: '出错了，请联系开发者！');
-  });
+    return true;
+  };
+  runApp(GetMaterialApp(
+    initialRoute: getInitialRoute(),
+    onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
+    builder: (context, child) {
+      final fontScale = Utils().prefUtil.getValue<double>('fontScale')!;
+      final colorScheme = Theme.of(context).colorScheme;
+      final mediaQuery = MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(fontScale)),
+        child: FToastBuilder()(context, child!),
+      );
+      // 根据平台决定是否需要 MoveWindow
+      final windowChild = (Platform.isWindows || Platform.isMacOS || Platform.isLinux)
+          ? Column(children: [WindowButtons(colorScheme: colorScheme), Expanded(child: mediaQuery)])
+          : mediaQuery;
+      return windowChild;
+    },
+    theme: Utils().themeUtil.buildTheme(Brightness.light),
+    darkTheme: Utils().themeUtil.buildTheme(Brightness.dark),
+    themeMode: ThemeMode.values[Utils().prefUtil.getValue<int>('themeMode')!],
+    defaultTransition: Transition.cupertino,
+    getPages: AppPages.routes,
+    localizationsDelegates: const [...AppLocalizations.localizationsDelegates, FlutterQuillLocalizations.delegate],
+    supportedLocales: AppLocalizations.supportedLocales,
+  ));
 }
