4b104af23a2bf9435d0959c129da0e61685d255d
ZhuJHua <1624109111@qq.com>
2024-12-14 20:03:21 +0800

    refactor(calendar): better calendar view
    Closes #72

--- lib/pages/home/calendar/calendar_logic.dart
+++ lib/pages/home/calendar/calendar_logic.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import '../../../utils/data/isar.dart';
 import 'calendar_state.dart';
@@ -7,40 +8,49 @@ import 'calendar_state.dart';
 class CalendarLogic extends GetxController {
   final CalendarState state = CalendarState();
 
+  late final ItemScrollController itemScrollController = ItemScrollController();
+  late final ScrollOffsetController scrollOffsetController = ScrollOffsetController();
+  late final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+  late final ScrollOffsetListener scrollOffsetListener = ScrollOffsetListener.create();
+
+  late final ExpansionTileController expansionTileController = ExpansionTileController();
+
   @override
   void onReady() async {
     await getMonthDiary(state.currentMonth.value);
+
     super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
   }
 
   // 获取当前月份的日记
   Future<void> getMonthDiary(DateTime value) async {
     state.isFetching.value = true;
-    final diaryList = await IsarUtil.getDiaryByMonth(value.year, value.month);
-    var dateWithDiaryList = <DateTime>[];
-    // 获取有日记的日期，只需要年月日
-    for (var diary in diaryList) {
-      // 如果不存在当前日期，则添加
-      var time = diary.time;
-      if (!dateWithDiaryList.contains(DateTime(time.year, time.month, time.day))) {
-        dateWithDiaryList.add(DateTime(time.year, time.month, time.day));
-      }
-    }
-    state.selectedDate.value = DateTime.now();
-    state.currentMonthDiaryMap = {for (var e in diaryList) e: GlobalKey()};
-    update();
+    state.currentMonth.value = value;
+    state.currentMonthDiaryList.value = await IsarUtil.getDiaryByMonth(value.year, value.month);
     state.isFetching.value = false;
   }
 
-  // 选中日期后滚动到该日期
-  Future<void> animateToSelectedDate(DateTime value) async {
-    state.selectedDate.value = value;
-    // 根据日期找到当前列表中第一篇日记对应的 GlobalKey，然后滚动到该日记
-    var key = state.currentMonthDiaryMap.entries
-        .firstWhere((element) =>
-            element.key.yMd == '${value.year.toString()}/${value.month.toString()}/${value.day.toString()}')
-        .value;
-    Scrollable.ensureVisible(key.currentContext!, duration: Duration(milliseconds: 300));
+  int _pendingScrollOperations = 0;
+
+  Future<void> animateToSelectedDateWithLock(DateTime value) async {
+    final index =
+        state.currentMonthDiaryList.indexWhere((element) => element.yMd == '${value.year}/${value.month}/${value.day}');
+    _pendingScrollOperations++;
+    state.isControllerScrolling.value = true;
+    try {
+      await itemScrollController.scrollTo(
+          index: index, duration: const Duration(seconds: 1), curve: Curves.easeInOutQuart);
+    } finally {
+      _pendingScrollOperations--;
+      if (_pendingScrollOperations == 0) {
+        state.isControllerScrolling.value = false;
+      }
+    }
   }
 
   // // 选中日期后重新获取日记
@@ -50,6 +60,10 @@ class CalendarLogic extends GetxController {
   // }
 
   void open(value) {
-    state.isExpanded.value = value;
+    if (value && !expansionTileController.isExpanded) {
+      expansionTileController.expand();
+    } else if (!value && expansionTileController.isExpanded) {
+      expansionTileController.collapse();
+    }
   }
 }
--- lib/pages/home/calendar/calendar_view.dart
+++ lib/pages/home/calendar/calendar_view.dart
@@ -2,133 +2,250 @@ import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:mood_diary/common/values/border.dart';
 import 'package:mood_diary/common/values/colors.dart';
 import 'package:mood_diary/components/diary_card/calendar_diary_card/calendar_diary_card_view.dart';
 import 'package:mood_diary/components/time_line/time_line_view.dart';
+import 'package:mood_diary/utils/array_util.dart';
+import 'package:rive_animated_icon/rive_animated_icon.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import 'calendar_logic.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
 
+  /// 根据当天日记的数量配置颜色的范围，从0到1
+  /// 0为最小值，1为最大值
+  /// 当天日记篇数为0时，返回0
+  /// 当前日记篇数为5时，返回1，即最大值，保留两位小数
+  double getDayColor({required int count}) {
+    if (count == 0) {
+      return 0;
+    }
+    if (count >= 5) {
+      return 1;
+    }
+    return count / 5;
+  }
+
+  Widget _buildActiveInfo({required Color lessColor, required Color moreColor, required TextStyle? textStyle}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        spacing: 2.0,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            '少',
+            style: textStyle,
+          ),
+          ...List.generate(5, (index) {
+            return Container(
+              width: 12.0,
+              height: 12.0,
+              decoration: BoxDecoration(
+                color: Color.lerp(lessColor, moreColor, (index + 1) / 5),
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+            );
+          }),
+          Text(
+            '多',
+            style: textStyle,
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final logic = Get.put(CalendarLogic());
     final state = Bind.find<CalendarLogic>().state;
     final colorScheme = Theme.of(context).colorScheme;
+    final textStyle = Theme.of(context).textTheme;
     final i18n = AppLocalizations.of(context)!;
 
     //生成日历选择器
     Widget buildDatePicker() {
-      final diaryList = state.currentMonthDiaryMap.keys.toList();
       var dateWithDiaryList = <DateTime>[];
+      var allDate = <DateTime>[];
       // 获取有日记的日期，只需要年月日
-      for (var diary in diaryList) {
+      for (var diary in state.currentMonthDiaryList) {
         // 如果不存在当前日期，则添加
         var time = diary.time;
+        allDate.add(DateTime(time.year, time.month, time.day));
         if (!dateWithDiaryList.contains(DateTime(time.year, time.month, time.day))) {
           dateWithDiaryList.add(DateTime(time.year, time.month, time.day));
         }
       }
+      final dateCountMap = ArrayUtil.countList(allDate);
 
-      return Obx(() {
-        return Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ClipRRect(
-            borderRadius: AppBorderRadius.mediumBorderRadius,
-            child: ExpansionPanelList(
-              expansionCallback: (index, isExpanded) {
-                logic.open(isExpanded);
-              },
-              children: [
-                ExpansionPanel(
-                    headerBuilder: (context, isExpanded) {
-                      return ListTile(
-                        title: Text(DateFormat.yMMMd().format(state.selectedDate.value)),
-                      );
-                    },
-                    backgroundColor: colorScheme.surfaceContainer,
-                    body: Obx(() {
-                      return CalendarDatePicker2(
-                        displayedMonthDate: state.currentMonth.value,
-                        config: CalendarDatePicker2Config(
-                            calendarViewMode: CalendarDatePicker2Mode.day,
-                            calendarType: CalendarDatePicker2Type.single,
-                            allowSameValueSelection: true,
-                            dayTextStylePredicate: ({required date}) {
-                              return dateWithDiaryList.any((dateTime) =>
-                                      dateTime.year == date.year &&
-                                      dateTime.month == date.month &&
-                                      dateTime.day == date.day)
-                                  ? TextStyle(
-                                      color: colorScheme.primary,
-                                    )
-                                  : null;
-                            }),
-                        onValueChanged: (value) async {
-                          await logic.animateToSelectedDate(value.first);
+      return Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: ExpansionTile(
+            title: Text(i18n.homeNavigatorCalendar),
+            initiallyExpanded: true,
+            collapsedShape: const RoundedRectangleBorder(borderRadius: AppBorderRadius.mediumBorderRadius),
+            shape: const RoundedRectangleBorder(borderRadius: AppBorderRadius.mediumBorderRadius),
+            backgroundColor: colorScheme.surfaceContainer,
+            collapsedBackgroundColor: colorScheme.surfaceContainerLow,
+            controller: logic.expansionTileController,
+            children: [
+              Stack(
+                children: [
+                  Obx(() {
+                    return CalendarDatePicker2(
+                      displayedMonthDate: state.currentMonth.value,
+                      config: CalendarDatePicker2Config(
+                        calendarViewMode: CalendarDatePicker2Mode.day,
+                        calendarType: CalendarDatePicker2Type.single,
+                        hideMonthPickerDividers: true,
+                        hideYearPickerDividers: true,
+                        useAbbrLabelForMonthModePicker: true,
+                        allowSameValueSelection: true,
+                        dayBuilder: ({
+                          required DateTime date,
+                          TextStyle? textStyle,
+                          BoxDecoration? decoration,
+                          bool? isSelected,
+                          bool? isDisabled,
+                          bool? isToday,
+                        }) {
+                          final contains = dateWithDiaryList.contains(date);
+                          final bgColor = contains
+                              ? Color.lerp(colorScheme.surfaceContainer, colorScheme.primary,
+                                  getDayColor(count: dateCountMap[date] ?? 0))
+                              : null;
+                          return Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: bgColor,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  date.day.toString(),
+                                  style: textStyle?.copyWith(
+                                    color: contains
+                                        ? ThemeData.estimateBrightnessForColor(bgColor!) == Brightness.dark
+                                            ? Colors.white
+                                            : Colors.black
+                                        : null,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
                         },
-                        onDisplayedMonthChanged: logic.getMonthDiary,
-                        value: [state.selectedDate.value],
-                      );
-                    }),
-                    isExpanded: state.isExpanded.value)
-              ],
-              expandedHeaderPadding: const EdgeInsets.all(8.0),
-              elevation: .0,
-            ),
+                        selectableDayPredicate: (DateTime date) {
+                          return dateWithDiaryList.contains(date);
+                        },
+                      ),
+                      onValueChanged: (value) {
+                        logic.animateToSelectedDateWithLock(value.first);
+                      },
+                      onDisplayedMonthChanged: logic.getMonthDiary,
+                      value: const [],
+                    );
+                  }),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: _buildActiveInfo(
+                        lessColor: colorScheme.surfaceContainer,
+                        moreColor: colorScheme.primary,
+                        textStyle: textStyle.labelSmall),
+                  ),
+                ],
+              ),
+            ],
           ),
-        );
-      });
+        ),
+      );
     }
 
     Widget buildCardList() {
-      // 日记列表
-      final diaryList = state.currentMonthDiaryMap.keys.toList();
-      // globalKey列表
-      final keyList = state.currentMonthDiaryMap.values.toList();
-      return SliverList.builder(
-        itemBuilder: (context, index) {
-          return TimeLineComponent(
-            key: keyList[index],
-            actionColor: Color.lerp(AppColor.emoColorList.first, AppColor.emoColorList.last, diaryList[index].mood)!,
-            child: CalendarDiaryCardComponent(diary: diaryList[index]),
-          );
+      double lastScrollOffset = 0.0;
+      return NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          if (notification is ScrollUpdateNotification) {
+            const double baseThreshold = 200.0; // 基础阈值
+            final double scrollDelta = notification.scrollDelta ?? 0.0; // 滚动增量（速度）
+
+            // 根据速度动态调整阈值，速度越快，阈值越高
+            final double dynamicThreshold = baseThreshold + (scrollDelta.abs() / 2);
+
+            final currentOffset = notification.metrics.pixels;
+            final distance = (currentOffset - lastScrollOffset).abs(); // 滚动距离
+
+            if (distance > dynamicThreshold) {
+              if (currentOffset > lastScrollOffset) {
+                // 向下滚动
+                if (!state.isControllerScrolling.value) logic.open(false);
+              } else if (currentOffset < lastScrollOffset) {
+                // 向上滚动
+                if (!state.isControllerScrolling.value) logic.open(true);
+              }
+              lastScrollOffset = currentOffset; // 更新滚动位置
+            }
+          }
+          return true;
         },
-        itemCount: diaryList.length,
+        child: Obx(() {
+          return ScrollablePositionedList.builder(
+            itemBuilder: (context, index) {
+              return TimeLineComponent(
+                actionColor: Color.lerp(
+                    AppColor.emoColorList.first, AppColor.emoColorList.last, state.currentMonthDiaryList[index].mood)!,
+                child: CalendarDiaryCardComponent(diary: state.currentMonthDiaryList[index]),
+              );
+            },
+            itemScrollController: logic.itemScrollController,
+            itemPositionsListener: logic.itemPositionsListener,
+            scrollOffsetController: logic.scrollOffsetController,
+            scrollOffsetListener: logic.scrollOffsetListener,
+            itemCount: state.currentMonthDiaryList.length,
+          );
+        }),
       );
     }
 
     return GetBuilder<CalendarLogic>(
       assignId: true,
       builder: (_) {
-        return CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              title: Text(i18n.homeNavigatorCalendar),
-              actions: [IconButton(onPressed: () async {}, icon: const Icon(Icons.today))],
+        return SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              spacing: 4.0,
+              children: [
+                Obx(() {
+                  return buildDatePicker();
+                }),
+                Expanded(child: Obx(() {
+                  return AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 400),
+                    child: state.isFetching.value
+                        ? Center(
+                            child: RiveAnimatedIcon(
+                            riveIcon: RiveIcon.search,
+                            height: 80,
+                            width: 80,
+                            loopAnimation: true,
+                            strokeWidth: 4,
+                            color: colorScheme.onSurface,
+                          ))
+                        : buildCardList(),
+                  );
+                })),
+              ],
             ),
-            SliverPadding(
-                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                sliver: SliverToBoxAdapter(
-                  child: buildDatePicker(),
-                )),
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              sliver: Obx(() {
-                return state.isFetching.value
-                    ? const SliverToBoxAdapter(
-                        child: Padding(
-                          padding: EdgeInsets.all(32.0),
-                          child: Center(child: CircularProgressIndicator()),
-                        ),
-                      )
-                    : buildCardList();
-              }),
-            )
-          ],
+          ),
         );
       },
     );
