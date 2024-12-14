ff1a8f5cc8e53e49799e766d2a03fe8f4cfbf446
ZhuJHua <1624109111@qq.com>
2024-12-14 20:00:27 +0800

    fix(lab): key did not update after input

--- lib/pages/laboratory/laboratory_view.dart
+++ lib/pages/laboratory/laboratory_view.dart
@@ -18,137 +18,139 @@ class LaboratoryPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('实验室'),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: const Text('腾讯云密钥'),
-            isThreeLine: true,
-            subtitle: SelectionArea(
-                child: Text(
-                    'ID:${PrefUtil.getValue<String>('tencentId') ?? ''}\nKey:${PrefUtil.getValue<String>('tencentKey') ?? ''}')),
-            trailing: IconButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextField(
-                                maxLines: 1,
-                                controller: logic.idTextEditingController,
-                                decoration: const InputDecoration(
-                                  labelText: 'ID',
-                                  border: OutlineInputBorder(),
+      body: GetBuilder<LaboratoryLogic>(builder: (_) {
+        return ListView(
+          children: [
+            ListTile(
+              title: const Text('腾讯云密钥'),
+              isThreeLine: true,
+              subtitle: SelectionArea(
+                  child: Text(
+                      'ID:${PrefUtil.getValue<String>('tencentId') ?? ''}\nKey:${PrefUtil.getValue<String>('tencentKey') ?? ''}')),
+              trailing: IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                TextField(
+                                  maxLines: 1,
+                                  controller: logic.idTextEditingController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'ID',
+                                    border: OutlineInputBorder(),
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 8.0,
-                              ),
-                              TextField(
-                                maxLines: 1,
-                                controller: logic.keyTextEditingController,
-                                decoration: const InputDecoration(
-                                  labelText: 'KEY',
-                                  border: OutlineInputBorder(),
+                                const SizedBox(
+                                  height: 8.0,
                                 ),
-                              ),
+                                TextField(
+                                  maxLines: 1,
+                                  controller: logic.keyTextEditingController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'KEY',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Get.backLegacy();
+                                  },
+                                  child: Text(i18n.cancel)),
+                              TextButton(
+                                  onPressed: () {
+                                    logic.setTencentID();
+                                  },
+                                  child: Text(i18n.ok))
                             ],
-                          ),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Get.backLegacy();
-                                },
-                                child: Text(i18n.cancel)),
-                            TextButton(
-                                onPressed: () {
-                                  logic.setTencentID();
-                                },
-                                child: Text(i18n.ok))
-                          ],
-                        );
-                      });
-                },
-                icon: const FaIcon(FontAwesomeIcons.wrench)),
-          ),
-          ListTile(
-            title: const Text('和风天气密钥'),
-            subtitle: SelectionArea(child: Text(PrefUtil.getValue<String>('qweatherKey') ?? '')),
-            trailing: IconButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: TextField(
-                            maxLines: 1,
-                            controller: logic.qweatherTextEditingController,
-                            decoration: const InputDecoration(
-                              labelText: 'Key',
-                              border: OutlineInputBorder(),
+                          );
+                        });
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.wrench)),
+            ),
+            ListTile(
+              title: const Text('和风天气密钥'),
+              subtitle: SelectionArea(child: Text(PrefUtil.getValue<String>('qweatherKey') ?? '')),
+              trailing: IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: TextField(
+                              maxLines: 1,
+                              controller: logic.qweatherTextEditingController,
+                              decoration: const InputDecoration(
+                                labelText: 'Key',
+                                border: OutlineInputBorder(),
+                              ),
                             ),
-                          ),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Get.backLegacy();
-                                },
-                                child: Text(i18n.cancel)),
-                            TextButton(
-                                onPressed: () {
-                                  logic.setQweatherKey();
-                                },
-                                child: Text(i18n.ok))
-                          ],
-                        );
-                      });
-                },
-                icon: const FaIcon(FontAwesomeIcons.wrench)),
-          ),
-          ListTile(
-            title: const Text('天地图密钥'),
-            subtitle: SelectionArea(child: Text(PrefUtil.getValue<String>('tiandituKey') ?? '')),
-            trailing: IconButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: TextField(
-                            maxLines: 1,
-                            controller: logic.tiandituTextEditingController,
-                            decoration: const InputDecoration(
-                              labelText: 'Key',
-                              border: OutlineInputBorder(),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Get.backLegacy();
+                                  },
+                                  child: Text(i18n.cancel)),
+                              TextButton(
+                                  onPressed: () {
+                                    logic.setQweatherKey();
+                                  },
+                                  child: Text(i18n.ok))
+                            ],
+                          );
+                        });
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.wrench)),
+            ),
+            ListTile(
+              title: const Text('天地图密钥'),
+              subtitle: SelectionArea(child: Text(PrefUtil.getValue<String>('tiandituKey') ?? '')),
+              trailing: IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: TextField(
+                              maxLines: 1,
+                              controller: logic.tiandituTextEditingController,
+                              decoration: const InputDecoration(
+                                labelText: 'Key',
+                                border: OutlineInputBorder(),
+                              ),
                             ),
-                          ),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Get.backLegacy();
-                                },
-                                child: Text(i18n.cancel)),
-                            TextButton(
-                                onPressed: () {
-                                  logic.setTiandituKey();
-                                },
-                                child: Text(i18n.ok))
-                          ],
-                        );
-                      });
-                },
-                icon: const FaIcon(FontAwesomeIcons.wrench)),
-          ),
-          ListTile(
-            onTap: () {
-              logic.exportErrorLog();
-            },
-            title: const Text('导出日志文件'),
-          ),
-        ],
-      ),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Get.backLegacy();
+                                  },
+                                  child: Text(i18n.cancel)),
+                              TextButton(
+                                  onPressed: () {
+                                    logic.setTiandituKey();
+                                  },
+                                  child: Text(i18n.ok))
+                            ],
+                          );
+                        });
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.wrench)),
+            ),
+            ListTile(
+              onTap: () {
+                logic.exportErrorLog();
+              },
+              title: const Text('导出日志文件'),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
