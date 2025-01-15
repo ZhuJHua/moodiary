09de3e1da018a952d3b89a47d592c6f0844b7e2d
ZhuJHua <1624109111@qq.com>
2025-01-15 23:58:20 +0800

    feat(feedback): add new feedback

--- lib/pages/web_view/web_view_view.dart
+++ lib/pages/web_view/web_view_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:mood_diary/common/values/colors.dart';
 import 'package:mood_diary/pages/web_view/web_view_state.dart';
 import 'package:refreshed/refreshed.dart';
 
@@ -8,11 +9,18 @@ import 'web_view_logic.dart';
 class WebViewPage extends StatelessWidget {
   const WebViewPage({super.key});
 
+  Widget _buildBackToHomeButton() {
+    return FilledButton.icon(
+        onPressed: Get.back,
+        label: const Text('退出反馈'),
+        icon: const Icon(Icons.outbound_rounded));
+  }
+
   @override
   Widget build(BuildContext context) {
     final WebViewLogic logic = Bind.find<WebViewLogic>();
     final WebViewState state = Bind.find<WebViewLogic>().state;
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final padding = MediaQuery.paddingOf(context);
 
     return PopScope(
       canPop: false,
@@ -21,41 +29,55 @@ class WebViewPage extends StatelessWidget {
         logic.handleBack();
       },
       child: Scaffold(
-        appBar: AppBar(
-            title: Text(state.title),
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back_rounded),
-              onPressed: Get.back,
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.refresh_rounded),
-                onPressed: logic.reload,
-              )
-            ],
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(2.0),
-              child: Obx(
-                () => LinearProgressIndicator(
-                  value: state.progress.value,
-                  backgroundColor: Colors.transparent,
-                  minHeight: 2,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(colorScheme.primary),
-                ),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                children: [
+                  Obx(() {
+                    return Container(
+                      height: padding.top,
+                      color: state.progress.value == 1
+                          ? AppColor.themeColorList[2]
+                          : null,
+                    );
+                  }),
+                  Expanded(
+                    child: InAppWebView(
+                      initialUrlRequest: URLRequest(url: WebUri(state.url)),
+                      initialSettings: logic.webSettings,
+                      pullToRefreshController: logic.pullToRefreshController,
+                      onWebViewCreated: (controller) {
+                        logic.webViewController = controller;
+                      },
+                      onProgressChanged: (controller, progress) {
+                        logic.onProgressChanged(progress);
+                      },
+                    ),
+                  ),
+                ],
               ),
-            )),
-        body: SafeArea(
-          child: InAppWebView(
-            initialUrlRequest: URLRequest(url: WebUri(state.url)),
-            initialSettings: logic.webSettings,
-            onWebViewCreated: (controller) {
-              logic.webViewController = controller;
-            },
-            onProgressChanged: (controller, progress) {
-              logic.onProgressChanged(progress);
-            },
-          ),
+            ),
+            Obx(() {
+              return Visibility(
+                visible: state.progress.value == 1,
+                child: Positioned(
+                  bottom: (state.isTop.value) ? null : 30,
+                  top: (state.isTop.value) ? 30 : null,
+                  right: (state.isRight.value) ? 30 : null,
+                  left: (state.isRight.value) ? null : 30,
+                  child: Draggable(
+                    feedback: _buildBackToHomeButton(),
+                    childWhenDragging: Container(),
+                    onDragEnd: (draggableDetails) {
+                      logic.updatePosition(draggableDetails, context);
+                    },
+                    child: _buildBackToHomeButton(),
+                  ),
+                ),
+              );
+            }),
+          ],
         ),
       ),
     );
