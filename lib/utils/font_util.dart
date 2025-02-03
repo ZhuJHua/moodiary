527767e1ab27e3accd0a0dec43cf4312c79655d7
ZhuJHua <1624109111@qq.com>
2025-02-04 05:05:45 +0800

    feat(font): add caching for font name and weight axis retrieval

--- lib/utils/font_util.dart
+++ lib/utils/font_util.dart
@@ -3,9 +3,15 @@ import 'dart:collection';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:moodiary/src/rust/api/font.dart';
+import 'package:moodiary/utils/lru.dart';
 
 class FontUtil {
   static final HashSet<String> _loadedFonts = HashSet();
+
+  static final _fontNameCache = AsyncLRUCache<String, String>(maxSize: 100);
+  static final _fontWghtAxisCache =
+      AsyncLRUCache<String, Map<String, double>>(maxSize: 100);
 
   static Future<bool> loadFont({
     required String fontName,
@@ -30,5 +36,31 @@ class FontUtil {
     } catch (e) {
       return false;
     }
+  }
+
+  static Future<String?> getFontName({required String filePath}) async {
+    final cacheName = await _fontNameCache.get(filePath);
+    if (cacheName != null) {
+      return cacheName;
+    }
+    final fontName = await FontReader.getFontNameFromTtf(ttfFilePath: filePath);
+    if (fontName != null) {
+      await _fontNameCache.put(filePath, fontName);
+    }
+    return fontName;
+  }
+
+  static Future<Map<String, double>> getFontWghtAxis(
+      {required String filePath}) async {
+    final cacheWghtAxis = await _fontWghtAxisCache.get(filePath);
+    if (cacheWghtAxis != null) {
+      return cacheWghtAxis;
+    }
+    final wghtAxis =
+        await FontReader.getWghtAxisFromVfFont(ttfFilePath: filePath);
+    if (wghtAxis.isNotEmpty) {
+      await _fontWghtAxisCache.put(filePath, wghtAxis);
+    }
+    return wghtAxis;
   }
 }
