cdf39298f9fd734a8cbc973a2415c9d3b47a2094
ZhuJHua <1624109111@qq.com>
2025-02-14 23:54:06 +0800

    feat: use the new route transition

--- lib/components/desktop_wrapper/background.dart
+++ lib/components/desktop_wrapper/background.dart
@@ -1,29 +1,62 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:moodiary/common/values/border.dart';
 
 class AdaptiveBackground extends StatelessWidget {
   final Widget child;
 
-  const AdaptiveBackground({super.key, required this.child});
+  const AdaptiveBackground({
+    super.key,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    final toolbarHeight = Platform.isMacOS ? 16.0 : 28.0;
     return LayoutBuilder(builder: (context, constraints) {
       final bool showBackground = constraints.maxWidth >= 512;
       if (!showBackground) return child;
       return Container(
         color: colorScheme.surfaceContainer,
-        padding: EdgeInsets.only(top: toolbarHeight, right: 16, bottom: 16),
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            borderRadius: AppBorderRadius.mediumBorderRadius,
+        padding: const EdgeInsets.only(
+          top: 8,
+          right: 8,
+          bottom: 8,
+        ),
+        child: ClipRRect(
+          borderRadius: AppBorderRadius.mediumBorderRadius,
+          child: ColoredBox(
             color: colorScheme.surface,
+            child: child,
           ),
+        ),
+      );
+    });
+  }
+}
+
+class PageAdaptiveBackground extends StatelessWidget {
+  final Widget child;
+
+  final bool isHome;
+
+  const PageAdaptiveBackground({
+    super.key,
+    required this.child,
+    this.isHome = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (isHome) return child;
+    final colorScheme = Theme.of(context).colorScheme;
+    return LayoutBuilder(builder: (context, constraints) {
+      final bool showBackground = constraints.maxWidth >= 600;
+      if (!showBackground) return child;
+      return Container(
+        color: colorScheme.surfaceContainer,
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: AppBorderRadius.mediumBorderRadius,
           child: child,
         ),
       );
--- lib/router/app_pages.dart
+++ lib/router/app_pages.dart
@@ -1,3 +1,8 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
+import 'package:moodiary/components/desktop_wrapper/background.dart';
+import 'package:moodiary/components/window_buttons/window_buttons.dart';
 import 'package:moodiary/pages/about/about_logic.dart';
 import 'package:moodiary/pages/about/about_view.dart';
 import 'package:moodiary/pages/agreement/agreement_view.dart';
@@ -43,6 +48,7 @@ import 'package:moodiary/pages/user/user_logic.dart';
 import 'package:moodiary/pages/user/user_view.dart';
 import 'package:moodiary/pages/video/video_logic.dart';
 import 'package:moodiary/pages/video/video_view.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:refreshed/refreshed.dart';
 
 import '../pages/sponsor/sponsor_logic.dart';
@@ -53,169 +59,199 @@ import 'app_routes.dart';
 class AppPages {
   static final List<GetPage> routes = [
     //启动页
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.startPage,
       page: () => const StartPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => StartLogic())],
     ),
     //首页路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.homePage,
       page: () => const HomePage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => HomeLogic())],
     ),
     //分析
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.analysePage,
       page: () => const AnalysePage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => AnalyseLogic())],
     ),
     //日记页路由
-    GetPage(
-      name: AppRoutes.diaryPage,
-      showCupertinoParallax: false,
-      page: () => DiaryDetailsPage(),
-    ),
+    MoodiaryGetPage(name: AppRoutes.diaryPage, page: () => DiaryDetailsPage()),
     //图片路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.photoPage,
       page: () => const ImagePage(),
-      popGesture: false,
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => ImageLogic())],
     ),
     //回收站
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.recyclePage,
       page: () => const RecyclePage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => RecycleLogic())],
     ),
     //登录路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.loginPage,
       page: () => const LoginPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => LoginLogic())],
     ),
     //新增/编辑日记路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.editPage,
       page: () => const EditPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => EditLogic())],
     ),
     //分享页路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.sharePage,
       page: () => const SharePage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => ShareLogic())],
     ),
     //字体页路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.fontPage,
       page: () => const FontPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => FontLogic())],
     ),
     //实验室路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.laboratoryPage,
       page: () => const LaboratoryPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => LaboratoryLogic())],
     ),
     //画画路由
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.drawPage,
       page: () => const DrawPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => DrawLogic())],
     ),
     //隐私政策
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.privacyPage,
       page: () => const PrivacyPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => PrivacyLogic())],
     ),
     //用户协议
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.agreementPage,
-      showCupertinoParallax: false,
       page: () => const AgreementPage(),
     ),
     //锁
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.lockPage,
       page: () => const LockPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => LockLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.userPage,
       page: () => const UserPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => UserLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.categoryManagerPage,
       page: () => const CategoryManagerPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => CategoryManagerLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.aboutPage,
       page: () => const AboutPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => AboutLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.videoPage,
-      popGesture: false,
       page: () => const VideoPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => VideoLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.mapPage,
       page: () => const MapPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => MapLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.diarySettingPage,
       page: () => const DiarySettingPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => DiarySettingLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.backupSyncPage,
       page: () => const BackupSyncPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => BackupSyncLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.assistantPage,
       page: () => const AssistantPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => AssistantLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.sponsorPage,
       page: () => const SponsorPage(),
-      showCupertinoParallax: false,
       binds: [Bind.lazyPut(() => SponsorLogic())],
     ),
-    GetPage(
+    MoodiaryGetPage(
       name: AppRoutes.webViewPage,
-      showCupertinoParallax: false,
       page: () => const WebViewPage(),
       binds: [Bind.lazyPut(() => WebViewLogic())],
     ),
   ];
+}
+
+class MoodiaryGetPage extends GetPage {
+  MoodiaryGetPage({
+    required super.name,
+    List<Bind> binds = const <Bind>[],
+    required Widget Function() page,
+  }) : super(
+         curve: Curves.linear,
+         page: () {
+           final body = PageAdaptiveBackground(
+             isHome: name == AppRoutes.homePage,
+             child: page(),
+           );
+           if (Platform.isAndroid || Platform.isIOS) {
+             return body;
+           } else {
+             return Column(
+               children: [const WindowsBar(), Expanded(child: body)],
+             );
+           }
+         },
+         customTransition: _MoodiaryPageTransition(),
+       );
+}
+
+class _MoodiaryPageTransition implements CustomTransition {
+  @override
+  Widget buildTransition(
+    BuildContext context,
+    Curve? curve,
+    Alignment? alignment,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    final pageRoute = ModalRoute.of(context) as PageRoute;
+    if (Platform.isAndroid) {
+      return const FadeForwardsPageTransitionsBuilder().buildTransitions(
+        pageRoute,
+        context,
+        animation,
+        secondaryAnimation,
+        child,
+      );
+    }
+    if (Platform.isIOS) {
+      return const CupertinoPageTransitionsBuilder().buildTransitions(
+        pageRoute,
+        context,
+        animation,
+        secondaryAnimation,
+        child,
+      );
+    }
+    return PageTransition(
+      type: PageTransitionType.fade,
+      child: child,
+    ).buildTransitions(context, animation, secondaryAnimation, child);
+  }
 }
